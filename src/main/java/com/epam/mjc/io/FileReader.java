7099da5cdbf6d4c0bbf8557b389091af553b0ff0
VLinkus <LinkusVyte@gmail.com>
2023-12-07 11:21:29 +0200

    M6 Task1 Refactored

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -9,40 +9,37 @@ import java.io.IOException;
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        FileInputStream input = null;
-        String text = "";
-        Profile profile = new Profile();
-        try {
-            input = new FileInputStream(file.getPath());
+        String text = getStringFromFile(file);
+        return getProfileInfoFromText(text);
+    }
+
+    private String getStringFromFile(File file){
+        StringBuilder text = new StringBuilder();
+        try (FileInputStream input = new FileInputStream(file.getPath());){
             int c;
             while ((c = input.read()) != -1)
-                text += (char) c;
-        } catch (FileNotFoundException e) {
-            throw new RuntimeException(e);
+                text.append((char) c);
         } catch (IOException e) {
             throw new RuntimeException(e);
-        } finally {
-                try {
-                    input.close();
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
         }
+        return text.toString();
+    }
+
+    public static Profile getProfileInfoFromText(String text){
+        Profile profile = new Profile();
         if(text.length() >1 ){
             String[] data = text.split("[\\u0020\\n\\r]");
             for (int i = 0; i<data.length; i++){
-                if(data[i].isEmpty()) {
+                if(data[i].isEmpty())
                     continue;
-                }
                 if(data[i].toLowerCase().contains("name")) {
-                    profile.setName(data[++i]);
+                    profile.setName(data[i+1]);
                 }else if(data[i].toLowerCase().contains("age")){
-                    profile.setAge(Integer.valueOf(data[++i]));
+                    profile.setAge(Integer.valueOf(data[i+1]));
                 }else if(data[i].toLowerCase().contains("email")){
-                    profile.setEmail(data[++i]);
+                    profile.setEmail(data[i+1]);
                 }else if(data[i].toLowerCase().contains("phone")){
-                    System.out.println(data[i]+" phone "+data[i]);
-                    profile.setPhone(Long.valueOf(data[++i]));
+                    profile.setPhone(Long.valueOf(data[i+1]));
                 }
             }
         }
